7666a8be75a24163aab9df854b2f156f203857bb
lebrancconvas <lebrancconvas@gmail.com>
2023-02-21 12:23:51 +0700

    🎉 feat(init): init project

---
+++ app/main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('mycanvas');
+const ctx = canvas.getContext('2d');
+
+// Set line width
+ctx.lineWidth = 10;
+
+// Wall
+ctx.strokeRect(75, 140, 150, 110);
+
+// Door
+ctx.fillRect(130, 190, 40, 60);
+
+// Roof
+ctx.beginPath();
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath();
+ctx.stroke();
